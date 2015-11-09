3b77281e7d979be0c8d058797ad02a88dcba67ef
mihawk <chan.sisowath@gmail.com>
2015-11-09 12:12:38 +0800

    SUI form

--- src/render/wf_event.erl
+++ src/render/wf_event.erl
@@ -11,6 +11,18 @@ new(P,E,D,N,Data,Source) -> new(P,E,D,N,Data,Source,<<>>).
 
 new(bin,Data) -> <<"ws.send(enc(tuple(atom('bin'),bin('",(nitro:pickle(Data))/binary,"'))));">>.
 new(undefined, _, _, _, _, _, _) -> <<>>;
+
+new({submitSemanticForm,Form}=Postback, Element, Delegate, Name, Data, Source, Validation) ->
+    Module = nitro:coalesce([Delegate, ?CTX#cx.module]),
+    Join=fun([]) -> []; ([E]) -> [$'|E]++[$'];
+        ([H|T]) -> [[$'|H]++[$']] ++ [ [$,,$'|E]++[$'] || E <- T ] end,
+    Event = #ev{name=Name, module=Module, msg=Postback, trigger=Element},
+    erlang:list_to_binary([ <<"{ if (validateSemanticForm('">>, nitro:to_binary(Form), <<"',[">>,
+    Join([ case is_atom(S) of true -> atom_to_list(S); false -> S end || S <- Source, S =/= []]),
+    <<"])) { ">>,nitro:to_binary(Validation),<<" ws.send(enc(tuple(atom('">>,
+    nitro:to_binary(application:get_env(n2o,event,pickle)),<<"'),bin('">>,Element,<<"'),bin('">>,
+    nitro:pickle(Event),<<"'),">>,Data,<<"))); } else console.log('Validation Error'); }">> ]);
+    
 new(Postback, Element, Delegate, Name, Data, Source, Validation) ->
     Module = nitro:coalesce([Delegate, ?CTX#cx.module]),
     Join=fun([]) -> []; ([E]) -> [$'|E]++[$'];
